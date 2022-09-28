0af414764853cc007a9975ec02cf8e5d1aefab9a
Gromyko Alexander <gromyko.alexander@gmail.com>
2022-09-28 21:34:48 +0400

    all done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,49 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        this.storage = new Resume[10000];
     }
 
     void save(Resume r) {
+        for (int i = 0; i < 10000; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < 10000; i++) {
+            if (storage[i] == null) return null;
+            else if (storage[i].uuid == uuid) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int size = this.size();
+        if (size != 0) {
+            for (int i = 0; i < 10000; i++) {
+                if (storage != null && storage[i].uuid == uuid) {
+                    storage[i] = storage[size - 1];
+                    storage[size - 1] = null;
+                    break;
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, this.size());
     }
 
     int size() {
-        return 0;
+        return (int) Arrays.stream(storage)
+                .filter(r -> r != null)
+                .count();
     }
 }
