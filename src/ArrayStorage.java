e0dcb0ac9f64fcd2f21f37a50dbfc5de64f199d5
Gromyko Alexander <gromyko.alexander@gmail.com>
2022-09-29 15:00:23 +0400

    use size() in most methods, fixed bugs

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,45 +7,35 @@ import java.util.Objects;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int storageSize;
+
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
+        for (int i = 0; i < size(); i++) {
                 storage[i] = null;
-            } else {
-                break;
             }
-        }
         storageSize = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                storageSize ++;
-                break;
-            }
+        if (size() < storage.length) {
+            storage[size()] = r;
+            storageSize ++;
         }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) return null;
-            else if (storage[i].uuid == uuid) return storage[i];
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid == uuid) return storage[i];
         }
         return null;
     }
 
     void delete(String uuid) {
-        int size = size();
-        if (size != 0) {
-            for (int i = 0; i < storage.length; i++) {
-                if (storage != null && storage[i].uuid == uuid) {
-                    storage[i] = storage[size - 1];
-                    storage[size - 1] = null;
-                    storageSize --;
-                    break;
-                }
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid == uuid) {
+                storage[i] = storage[size() - 1];
+                storage[size() - 1] = null;
+                storageSize --;
+                break;
             }
         }
     }
