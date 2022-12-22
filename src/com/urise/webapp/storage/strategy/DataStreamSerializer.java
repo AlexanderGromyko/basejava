70321d214022f0335979595c27e871dffc09f88b
Gromyko Alexander <gromyko.alexander@gmail.com>
2022-12-22 16:03:19 +0400

    HW10 done. Some minor refactoring in DataStreamSerializer

--- src/com/urise/webapp/storage/strategy/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/strategy/DataStreamSerializer.java
@@ -11,34 +11,31 @@ import java.util.Map;
 public class DataStreamSerializer implements SerializerStrategy {
     @Override
     public void doWrite(Resume resume, OutputStream os) throws IOException {
-        try(DataOutputStream dos = new DataOutputStream(os)){
+        try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
             dos.writeInt(resume.getContacts().size());
-            for (Map.Entry<ContactType, String> entry: resume.getContacts().entrySet()) {
+            for (Map.Entry<ContactType, String> entry : resume.getContacts().entrySet()) {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             }
-            writeSection(SectionType.PERSONAL, resume, dos);
-            writeSection(SectionType.OBJECTIVE, resume, dos);
-            writeSection(SectionType.ACHIEVEMENTS, resume, dos);
-            writeSection(SectionType.QUALIFICATIONS, resume, dos);
-            writeSection(SectionType.EXPERIENCE, resume, dos);
-            writeSection(SectionType.EDUCATION, resume, dos);
+            for (SectionType sectionType : SectionType.values()) {
+                writeSection(sectionType, resume, dos);
+            }
         }
     }
 
     @Override
     public Resume doRead(InputStream is) throws IOException {
-        try(DataInputStream dis = new DataInputStream(is)){
+        try (DataInputStream dis = new DataInputStream(is)) {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
             int size = dis.readInt();
-            for(int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 resume.setContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
             }
-            for(int i = 0; i < 6; i++) {
+            for (int i = 0; i < 6; i++) {
                 readSection(SectionType.valueOf(dis.readUTF()), resume, dis);
             }
             return resume;
@@ -46,7 +43,7 @@ public class DataStreamSerializer implements SerializerStrategy {
     }
 
     private void writeSection(SectionType sectionType, Resume resume, DataOutputStream dos) throws IOException {
-        switch(sectionType) {
+        switch (sectionType) {
             case PERSONAL, OBJECTIVE -> {
                 dos.writeUTF(sectionType.name());
                 dos.writeUTF(resume.getSection(sectionType).toString());
@@ -80,14 +77,14 @@ public class DataStreamSerializer implements SerializerStrategy {
     }
 
     private void readSection(SectionType sectionType, Resume resume, DataInputStream dis) throws IOException {
-        switch(sectionType) {
+        switch (sectionType) {
             case PERSONAL, OBJECTIVE -> {
                 resume.setSection(sectionType, new TextSection(dis.readUTF()));
             }
             case ACHIEVEMENTS, QUALIFICATIONS -> {
                 ListSection listSection = new ListSection(new ArrayList<>());
                 int size = dis.readInt();
-                for(int i = 0; i < size; i++) {
+                for (int i = 0; i < size; i++) {
                     listSection.addString(dis.readUTF());
                 }
                 resume.setSection(sectionType, listSection);
@@ -95,10 +92,10 @@ public class DataStreamSerializer implements SerializerStrategy {
             case EXPERIENCE, EDUCATION -> {
                 OrganizationSection organizationSection = new OrganizationSection(new ArrayList<>());
                 int size = dis.readInt();
-                for(int i = 0; i < size; i++) {
+                for (int i = 0; i < size; i++) {
                     Organization organization = new Organization(dis.readUTF(), dis.readUTF());
                     int periodsSize = dis.readInt();
-                    for(int y = 0; y < periodsSize; y++) {
+                    for (int y = 0; y < periodsSize; y++) {
                         organization.addPeriod(new Period(LocalDate.parse(dis.readUTF()),
                                 LocalDate.parse(dis.readUTF()),
                                 dis.readUTF(),
