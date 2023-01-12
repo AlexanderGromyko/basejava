62205e8b00c3e16fec7cc9c5bc471b2b25d951fa
Gromyko Alexander <gromyko.alexander@gmail.com>
2023-01-12 12:16:36 +0400

    HW13 done. All done.

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -13,19 +13,13 @@ public class Config {
     private static final Config INSTANCE = new Config();
     private Properties properties = new Properties();
     private File storageDir;
-    private String dbUrl;
-    private String dbUser;
-    private String dbPassword;
     private SqlStorage storage;
 
     private Config() {
         try (InputStream is = new FileInputStream(PROPERTIES)) {
             properties.load(is);
             storageDir = new File(properties.getProperty("storage.dir"));
-            dbUrl = properties.getProperty("db.url");
-            dbUser = properties.getProperty("db.user");
-            dbPassword = properties.getProperty("db.password");
-            storage = new SqlStorage(dbUrl, dbUser, dbPassword);
+            storage = new SqlStorage(properties.getProperty("db.url"), properties.getProperty("db.user"), properties.getProperty("db.password"));
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config fil " + PROPERTIES.getAbsolutePath());
         }
@@ -37,18 +31,6 @@ public class Config {
 
     public SqlStorage getStorage() {
         return storage;
-    }
-
-    public String getDbUrl() {
-        return dbUrl;
-    }
-
-    public String getDbUser() {
-        return dbUser;
-    }
-
-    public String getDbPassword() {
-        return dbPassword;
     }
 
     public File getStorageDir() {
